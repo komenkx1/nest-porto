7d822f476678f768cbf22c88788774f9610a3921
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2024-01-05 07:13:26 +0800

    test to disable public

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,16 @@ import { UserModule } from './user/user.module';
 import { PortofolioModule } from './portofolio/portofolio.module';
 import { CertificateModule } from './certificate/certificate.module';
 import { CategoryModule } from './category/category.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
+// import { join } from 'path';
 import { TagModule } from './tag/tag.module';
 import { PortofolioTagModule } from './portofolio_tag/portofolio_tag.module';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..'), // Sesuaikan dengan struktur direktori Anda
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..'), // Sesuaikan dengan struktur direktori Anda
+    // }),
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UserModule,

a17808c1dfc65c7f9b25edc933a4ace455418326
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2024-01-05 08:41:58 +0800

    update directory

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { Category } from './category.entity';
-import { ApiResponse } from 'src/api-response';
+import { ApiResponse } from '../api-response';
 import { CategoryDto } from './category.dto';
 
 @Controller()

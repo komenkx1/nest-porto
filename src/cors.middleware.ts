3ca9c0cf6123a2aae7f0b0a672da0e9a9a948fbf
I komang wahyu hadi permana <44283571+komenkx1@users.noreply.github.com>
2024-01-07 05:54:12 +0800

    try to disable cors

--- src/cors.middleware.ts
+++ src/cors.middleware.ts
@@ -11,7 +11,10 @@ export class CorsMiddleware implements NestMiddleware {
       'Access-Control-Allow-Headers',
       'Origin, X-Requested-With, Content-Type, Accept',
     );
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS',
+    );
     next();
   }
 }
